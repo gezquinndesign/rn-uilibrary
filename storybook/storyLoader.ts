cf2522cfd32f897328c52f9fa1216a50656068f7
Gez Quinn <holla@gezquinn.design>
2022-04-10 11:01:40 +0100

    convert to ts

---
+++ storybook/storyLoader.ts
@@ -0,0 +1,25 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/button/button.stories')
+  require('../src/components/StyledComponent/StyledComponent.stories')
+  require('../src/components/StyledPressable/StyledPressable.stories')
+  require('../src/components/TestComponent1/TestComponent1.stories')
+  require('../src/components/TestComponent2/TestComponent2.stories')
+}
+
+const stories = [
+  '../src/components/button/button.stories',
+  '../src/components/StyledComponent/StyledComponent.stories',
+  '../src/components/StyledPressable/StyledPressable.stories',
+  '../src/components/TestComponent1/TestComponent1.stories',
+  '../src/components/TestComponent2/TestComponent2.stories',
+]
+
+module.exports = {
+  loadStories,
+  stories,
+}
