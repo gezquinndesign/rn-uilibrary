6e72d52f10bb410cbbb7157cdf1b87af84700935
Gez Quinn <holla@gezquinn.design>
2022-04-11 12:31:31 +0100

    import before export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as styles from './shared/styles'
 import * as global from './shared/global'
 import * as animation from './shared/animation'
 import * as icons from './shared/icons'
+import Button from './components/button/button'
 
-export { styles, global, animation, icons }
-
-export * as Button from './components/button/button'
+export { styles, global, animation, icons, Button }
