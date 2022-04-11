cf46cf3af5ab26dfe36b1324feab3e528f750005
Gez Quinn <holla@gezquinn.design>
2022-04-11 03:06:47 +0100

    distribution setup

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['babel-plugin-styled-components'],
+  plugins: [
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    'babel-plugin-styled-components',
+  ],
 }
