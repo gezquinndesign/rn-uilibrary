ec2299ceb4082787880fa775b88addf04b3804e9
Gez Quinn <holla@gezquinn.design>
2022-04-11 13:10:53 +0100

    exports

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import Button from './Button'
-import StyledComponent from './StyledComponent'
-import StyledPressable from './StyledPressable'
-import TestComponent1 from './TestComponent1'
-import TestComponent2 from './TestComponent2'
-
-export { Button, StyledComponent, StyledPressable, TestComponent1, TestComponent2 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import * as styles from './shared/styles'
 import * as global from './shared/global'
 import * as animation from './shared/animation'
 import * as icons from './shared/icons'
-import { Button, StyledComponent, StyledPressable, TestComponent1, TestComponent2 } from './components'
+import Button from './components/Button/Button'
+import StyledComponent from './components/StyledComponent/StyledComponent'
+import StyledPressable from './components/StyledPressable/StyledPressable'
+import TestComponent1 from './components/TestComponent1/TestComponent1'
+import TestComponent2 from './components/TestComponent2/TestComponent2'
 
 export { styles, global, animation, icons, Button, StyledComponent, StyledPressable, TestComponent1, TestComponent2 }
