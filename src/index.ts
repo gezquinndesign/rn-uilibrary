a6ab27fc48ebb2f819890393b20be505f75b5cdc
Gez Quinn <holla@gezquinn.design>
2022-04-11 15:08:31 +0100

    named exports

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import * as styles from './shared/styles'
 import * as global from './shared/global'
 import * as animation from './shared/animation'
 import * as icons from './shared/icons'
-import Button from './components/Button/Button'
-import StyledComponent from './components/StyledComponent/StyledComponent'
-import StyledPressable from './components/StyledPressable/StyledPressable'
-import TestComponent1 from './components/TestComponent1/TestComponent1'
-import TestComponent2 from './components/TestComponent2/TestComponent2'
 
-export { styles, global, animation, icons, Button, StyledComponent, StyledPressable, TestComponent1, TestComponent2 }
+export { styles, global, animation, icons }
+
+export { Button, StyledComponent, StyledPressable, TestComponent1, TestComponent2 } from './components'
