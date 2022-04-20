d7ae9e49ca95a54e1f54cac875360e7f3d2d096f
Dan Kennedy <dannykennedy@email.com>
2022-04-20 16:21:32 +0100

    Update storyLoader.ts

--- storybook/storyLoader.ts
+++ storybook/storyLoader.ts
@@ -4,7 +4,7 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('../src/components/button/button.stories')
+  require('../src/components/Button/Button.stories')
   require('../src/components/StyledComponent/StyledComponent.stories')
   require('../src/components/StyledPressable/StyledPressable.stories')
   require('../src/components/TestComponent1/TestComponent1.stories')
@@ -12,7 +12,7 @@ function loadStories() {
 }
 
 const stories = [
-  '../src/components/button/button.stories',
+  '../src/components/Button/Button.stories',
   '../src/components/StyledComponent/StyledComponent.stories',
   '../src/components/StyledPressable/StyledPressable.stories',
   '../src/components/TestComponent1/TestComponent1.stories',
